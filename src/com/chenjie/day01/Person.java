5d5a3a702fdc4a1999ad34900ab1cb3a783f09f2
chenjie <1055939306@qq.com>
2022-09-29 16:45:43 +0800

    第一次提交

---
+++ src/com/chenjie/day01/Person.java
@@ -0,0 +1,13 @@
+package com.chenjie.day01;
+
+public class Person {
+    private  String name;
+    private  String age;
+
+    public static void main(String[] args) {
+        eat();
+    }
+    public static void eat(){
+        System.out.println("吃东西");
+    }
+}
